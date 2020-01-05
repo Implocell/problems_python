2419c8f682f58c65dc7a8eb903a049d4008968f3
Thomas <thomasgran@thomass-macbook-pro.home>
2020-01-05 14:40:08 +0100

    first draft, while watching tv

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,2 +1,54 @@
 import csv
-import os
+from time import time
+import argparse
+
+parser = argparse.ArgumentParser('Quiz Game')
+parser.add_argument('-csv', action='store', dest='csv', default="problems.csv",
+                    help="csv file in the format of 'question, answer'")
+parser.add_argument('-timer', action='store', dest='timer', default=30,
+                    help="Seconds for timer", type=int)
+
+results = parser.parse_args()
+csv_file = results.csv
+time_limit = results.timer
+
+with open(csv_file) as f:
+    csv_reader = csv.reader(f, delimiter=",")
+    problems = {}
+    for row in csv_reader:
+        problems[row[0]] = row[1]
+
+
+points = 0
+timer = time() + time_limit
+
+time_left = 1
+
+for k, v in problems.items():
+    if time_left:
+        print(k)
+        a = input()
+        if a == v:
+            points += 1
+        if time() > timer:
+            time_left = 0
+    else:
+        break
+
+if time_left == 0:
+    print("Time ran out")
+
+print(f'{points} points from game')
+print(timer)
+
+'''
+1+2,3
+8+6,14
+3+1,4
+1+4,5
+5+1,6
+2+3,5
+3+3,6
+2+4,6
+5+2,7
+'''
